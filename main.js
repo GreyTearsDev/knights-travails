817f19555d9544c07132128f29a0998321b056e2
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-02-10 10:33:28 +0100

    Define the knight moves

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 'use strict'
 
 const chessboard = Array(8).fill().map(() => Array(8).fill(false));
+const knightMoves = [
+  [-2, -1],
+  [-2, 1],
+  [-1, -2],
+  [-1, 2],
+  [1, -2],
+  [1, 2],
+  [2, -1],
+  [2, 1],
+]

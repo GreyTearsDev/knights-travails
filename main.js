44fa261e93c4422a6320a8432e07f9c502502cb4
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-02-10 10:46:04 +0100

    Define 'getValidMoves()' method

--- main.js
+++ main.js
@@ -10,4 +10,20 @@ const knightMoves = [
   [1, 2],
   [2, -1],
   [2, 1],
-]
+];
+
+function getValidMoves(row, col) {
+  const validMoves = [];
+  
+  /* deltaX represents the change in row and deltaY 
+  represents the change in column*/
+  for (const [deltaX, deltaY] of knightMoves) {
+    const nextRow = row + deltaX;
+    const nextCol = col + deltaY;
+    if (nextRow >= 0 && nextRow < 8 && nextCol >= 0 && nextCol < 8) {
+      validMoves.push([nextRow, nextCol]);
+    }
+  }
+
+  return validMoves;
+}
